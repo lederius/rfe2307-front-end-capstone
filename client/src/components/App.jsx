1601c075bffd79dca377b92bb7c79dc4f3cc16a0
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-08-10 14:25:34 -0500

    updated webpack, created component folders, created appComponent

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  return (
+    <div>
+      React is running! Great job team! Now go to work.....
+    </div>
+  );
+};
+
+export default App;
