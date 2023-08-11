2d99791c4b06b58f2290a9367eaaec7e80c66ca8
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-08-11 14:23:04 -0500

    routing implemented for qa/question/product_id

--- client/src/components/questionsAnswers/QuestionsList.jsx
+++ client/src/components/questionsAnswers/QuestionsList.jsx
@@ -1,12 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Question from './Question.jsx';
+import axios from 'axios';
+
 const QuestionsList = () => {
+
+  const id = 37315;
+  const [questions, setQuestions] = useState([]);
+
+  const getQuestions = () => {
+    // eslint-disable-next-line camelcase
+    axios.get(`/qa/questions/${id}`, {params: {product_id: id}})
+      .then((data) => {
+        // console.log('productInfo', data.data.results);
+        setQuestions(data.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+
+
   return (
     <div>
-      QuestionsList Component
-      <Question/>
-      <Question/>
-      <Question/>
+      <h2 className="text-2xl font-bold">QuestionsList Component</h2>
+      {questions.map((question, index) => {
+        return <Question question={question} key={index}/>;
+      })}
+
     </div>
   );
 };
