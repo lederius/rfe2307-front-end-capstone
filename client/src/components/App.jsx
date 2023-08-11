4e586b7d626a4dfaf6180d9fd93aa618747e1be5
Aslambek Baimouradov <zandak@gmail.com>
2023-08-11 19:03:43 -0400

    some css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
       {/* <ProductDetails /> */}
       {/* <QuestionsList /> */}
       {/* <RatingReviews /> */}
-      {/* <RelatedProducts /> */}
+      <RelatedProducts />
     </div>
   );
 };
---
+++ client/src/components/relatedProducts/productCards.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './related.css';
+
+const ProductCard = () => {
+
+  return (
+    <aside className="card display: block; position: absolute; width: 483px; height: 1315px;">
+      <img />
+      <h2>CATEGORY</h2>
+      <h3>Product Title</h3>
+      <p>$$$</p>
+      Rating
+    </aside>
+  );
+};
+
+export default ProductCard;
+
