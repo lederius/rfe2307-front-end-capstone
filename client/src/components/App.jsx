0c4969e2dede7552722587c8b792dfa856ac3a60
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-08-10 19:04:03 -0500

    Fixed tailwind issue not applying css.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,14 +4,13 @@ const App = () => {
 
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">
       React is running! Great job team! Now go to work.....
+      </h1>
       {/* <productDetails /> */}
       <QuestionsList />
       {/* <ratingReviews /> */}
       {/* <relatedProducts /> */}
-      <h1 class="text-3xl font-bold underline">
-      React is running! Great job team! Now go to work.....
-      </h1>
     </div>
   );
 };
