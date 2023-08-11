8a85b6824c3b4d85aea680361047404da1c4da0b
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-08-10 19:35:13 -0500

    Fixed reactDom error. Added Question component.

--- client/src/components/questionsAnswers/QuestionsList.jsx
+++ client/src/components/questionsAnswers/QuestionsList.jsx
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
-
+import Question from './Question.jsx';
 const QuestionsList = () => {
   return (
     <div>
       QuestionsList Component
+      <Question/>
+      <Question/>
+      <Question/>
     </div>
   );
 };
