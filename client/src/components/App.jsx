cc15cb0895fbadc84b260a9e0df338b7417f2973
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-08-10 19:36:40 -0500

    Commented component in App.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
       React is running! Great job team! Now go to work.....
       </h1>
       {/* <productDetails /> */}
-      <QuestionsList />
+      {/* <QuestionsList /> */}
       {/* <ratingReviews /> */}
       {/* <relatedProducts /> */}
     </div>
