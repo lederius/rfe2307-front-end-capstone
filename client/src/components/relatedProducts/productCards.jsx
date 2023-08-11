d8fbe735ca83f29993961777810fbc7156fa735d
Aslambek Baimouradov <zandak@gmail.com>
2023-08-11 19:57:43 -0400

    implemented css

--- client/src/components/relatedProducts/productCards.jsx
+++ client/src/components/relatedProducts/productCards.jsx
@@ -4,13 +4,15 @@ import './related.css';
 const ProductCard = () => {
 
   return (
-    <aside className="card display: block; position: absolute; width: 483px; height: 1315px;">
-      <img />
-      <h2>CATEGORY</h2>
-      <h3>Product Title</h3>
-      <p>$$$</p>
+    <div className='productCard'>
+      <img src="img.png" />
+      <div className='container'>
+        <h2>CATEGORY</h2>
+        <h3><b>Product Title</b></h3>
+        <p>$$$</p>
       Rating
-    </aside>
+      </div>
+    </div>
   );
 };
 
