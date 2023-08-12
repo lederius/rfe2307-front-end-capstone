a34034a9bc26b13b3a91c2112e774ccc07c179e7
Aslambek Baimouradov <zandak@gmail.com>
2023-08-11 23:33:09 -0400

    updated css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
       {/* <ProductDetails /> */}
       {/* <QuestionsList /> */}
       {/* <RatingReviews /> */}
-      <RelatedProducts />
+      {/* <RelatedProducts /> */}
     </div>
   );
 };
