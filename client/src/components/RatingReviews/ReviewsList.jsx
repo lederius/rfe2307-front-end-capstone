f1f3f78c4a8964e6bf40999a310848351a0fe41d
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-08-14 17:54:59 +0100

    added buttons, brainstorming how to render 2/time

--- client/src/components/RatingReviews/ReviewsList.jsx
+++ client/src/components/RatingReviews/ReviewsList.jsx
@@ -79,31 +79,52 @@ const ReviewsList = ({ reviewList }) => {
           </label>
           <label>
 
-          </label>
-        </form>
-      </div>
-    );
-  };
+  const [reviewList, setReviewList] = useState([]);
+  const [childReviews, setChildReviews] = useState([]);
+  // console.log(reviewList, 'child', childReviews)
+  // pass 2 reviews first
+  // render more reviews if applicable
+    // on click pass two more
+    // when no more -> button disappears
+  // create form for new review button
+
+  useEffect(() => {
+    fetch();
+  }, [])
+
+  // add second useEffect for childReviews
+
+  const fetch = () => {
+    axios.get(`/reviews/${id}`, { params: { productID: id } })
+      .then(res => setReviewList(res.data))
+      .catch(err => console.log('failed client get req', err));
+  }
+
+  const moreClick = () => {
+    // hide all and then reveal two at a time
+  }
+
+  const addClick = () => {
+
+  }
 
   return (
     <div>
       {/* cond render if reviews avail */}
-      {reviewList.length > 0 ?
-        <div>
-          <SingleReview reviewList={reviewList} />
-          <div className="flex justify-between">
-            {visibleReviews < reviewList.length && (
-              <button role='more' className="bg-slate-200 hover:bg-slate-400 font-bold py-2 px-4 rounded shadow-lg" onClick={moreClick}>MORE REVIEWS</button>
-            )}
-            <button className="bg-slate-200 hover:bg-slate-400 font-bold py-2 px-4 rounded shadow-lg" onClick={addClick}>ADD A REVIEW +</button>
-          </div>
+      {reviewList ?
+      <div>
+        <SingleReview reviewList={reviewList}/>
+        <div className="flex justify-between">
+          <button className="bg-slate-200 hover:bg-slate-400 font-bold py-2 px-4 rounded shadow-lg" onClick={moreClick}>MORE REVIEWS</button>
+          <button className="bg-slate-200 hover:bg-slate-400 font-bold py-2 px-4 rounded shadow-lg" onClick={addClick}>ADD A REVIEW +</button>
         </div>
-        :
-        <div>
-          <div className="flex justify-between">
-            <button role='add' className="bg-slate-200 hover:bg-slate-400 font-bold py-2 px-4 rounded shadow-lg" onClick={addClick}>ADD A REVIEW +</button>
-          </div>
-        </div>}
+      </div>
+      :
+      <div>
+        <div className="flex justify-between">
+          <button className="bg-slate-200 hover:bg-slate-400 font-bold py-2 px-4 rounded shadow-lg">ADD A REVIEW +</button>
+        </div>
+      </div>}
     </div>
   )
 }
