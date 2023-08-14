0d9fd72f0a6fc5ddd85966a257841def620bfcb2
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-08-14 17:54:59 +0100

    added buttons, brainstorming how to render 2/time

--- client/src/components/RatingReviews/SingleReview.jsx
+++ client/src/components/RatingReviews/SingleReview.jsx
@@ -68,11 +68,45 @@ const SingleReview = ({ review }) => {
                   <div className='body p-1'>{review.body}</div>
                   <div className='rec p-1'>{rec(review.recommend)}</div>
                   <div className='photos flex gap-3 p-1'>{photo(review.photos)}</div>
+    <div className='oneReview'>
+
+      <div className='numReviews'>
+        {reviewList.length} reviews, sorted by
+        <select className='sort'>
+          <option>Relevance</option>
+          <option>Newest</option>
+          <option>Helpful</option>
+        </select>
+      </div>
+
+      <div className='reviewTile rounded-lg shadow-xl'>
+          {reviewList.map(review => {
+
+            const [helpCount, setHelpCount] = useState(review.helpfulness);
+
+            const handleClick = (e, count) => {
+              e.preventDefault();
+              setHelpCount(helpCount + 1);
+              // add post request here for persistency
+            }
+
+            return (
+              <div key={review.review_id} className='min-h-[15%] border-b-2 border-black'>
+                <div className="flex justify-between items-center pt-4 px-2">
+                  <span className='stars'>{stars(review.rating)}</span>
+                  <span className='usernameDate'>{review.reviewer_name}, {moment(review.date).fromNow()}</span>
+                </div>
+                <div className="p-2">
+                  <div className='summary text-lg font-bold p-1'>{review.summary}</div>
+                  <div className='body p-1'>{review.body}</div>
+                  <div className='rec p-1'>{rec(review.recommend)}</div>
+                  <div className='photos flex gap-3 p-1'>{photo(review.photos)}</div>
 
                   {/* format this when new review + post req is setup */}
                   {/* <div className='response'>{review.response}</div> */}
 
-                <div className='help pb-8 text-sm p-1'>Did you find this review helpful? <button role="helpful" className='underline' onClick={e => handleClick(e, review.review_id.toString(), review.helpfulness)}>Yes ({review.helpfulness})</button></div>
+                  <div className='help pb-8 text-sm p-1'>Did you find this review helpful? <button data-testid="helpful" onClick={(e) => handleClick(e, helpCount)}>Yes ({helpCount})</button></div>
+                </div>
               </div>
             )
           }
--- client/src/components/RatingReviews/rr.test.js
+++ client/src/components/RatingReviews/rr.test.js
@@ -1,87 +1,94 @@
+const request = require("supertest");
+const assert = require("assert");
+require("dotenv").config();
+
+// import {screen, mount} from '@testing-library/react';
+// import React from 'react';
+// import SingleReview from './SingleReview.jsx';
 
 describe('API calls', () => {
-
+  // var testfunc = function() { return 'Hello'; };
   it ('Should fetch correct reviews data based on product ID', () => {
     const expectedData = [
       {
-        'review_id': 1280180,
-        'rating': 5,
-        'summary': 'squidward test review',
-        'recommend': true,
-        'response': null,
-        'body': 'just a test haha',
-        'date': '2023-06-26T00:00:00.000Z',
-        'reviewer_name': 'squid',
-        'helpfulness': 29,
-        'photos': [
+        "review_id": 1280180,
+        "rating": 5,
+        "summary": "squidward test review",
+        "recommend": true,
+        "response": null,
+        "body": "just a test haha",
+        "date": "2023-06-26T00:00:00.000Z",
+        "reviewer_name": "squid",
+        "helpfulness": 29,
+        "photos": [
           {
-            'id': 2459025,
-            'url': 'https://static.wikia.nocookie.net/spongebob/images/4/4f/The_Two_Faces_of_Squidward_075.png'
+            "id": 2459025,
+            "url": "https://static.wikia.nocookie.net/spongebob/images/4/4f/The_Two_Faces_of_Squidward_075.png"
           },
           {
-            'id': 2459026,
-            'url': 'https://static.wikia.nocookie.net/spongebob/images/9/96/The_Two_Faces_of_Squidward_174.png'
+            "id": 2459026,
+            "url": "https://static.wikia.nocookie.net/spongebob/images/9/96/The_Two_Faces_of_Squidward_174.png"
           }
         ]
       },
       {
-        'review_id': 1135534,
-        'rating': 2,
-        'summary': 'is this working??',
-        'recommend': true,
-        'response': null,
-        'body': 'test 111111',
-        'date': '2022-02-11T00:00:00.000Z',
-        'reviewer_name': 'oliver6666',
-        'helpfulness': 17,
-        'photos': []
+        "review_id": 1135534,
+        "rating": 2,
+        "summary": "is this working??",
+        "recommend": true,
+        "response": null,
+        "body": "test 111111",
+        "date": "2022-02-11T00:00:00.000Z",
+        "reviewer_name": "oliver6666",
+        "helpfulness": 17,
+        "photos": []
       },
       {
-        'review_id': 1135535,
-        'rating': 2,
-        'summary': 'is this working??',
-        'recommend': true,
-        'response': null,
-        'body': 'test 111111',
-        'date': '2022-02-11T00:00:00.000Z',
-        'reviewer_name': 'oliver7777',
-        'helpfulness': 4,
-        'photos': []
+        "review_id": 1135535,
+        "rating": 2,
+        "summary": "is this working??",
+        "recommend": true,
+        "response": null,
+        "body": "test 111111",
+        "date": "2022-02-11T00:00:00.000Z",
+        "reviewer_name": "oliver7777",
+        "helpfulness": 4,
+        "photos": []
       },
       {
-        'review_id': 1280179,
-        'rating': 5,
-        'summary': 'squidward test review',
-        'recommend': true,
-        'response': null,
-        'body': 'just a test haha',
-        'date': '2023-06-26T00:00:00.000Z',
-        'reviewer_name': 'squid',
-        'helpfulness': 2,
-        'photos': [
+        "review_id": 1280179,
+        "rating": 5,
+        "summary": "squidward test review",
+        "recommend": true,
+        "response": null,
+        "body": "just a test haha",
+        "date": "2023-06-26T00:00:00.000Z",
+        "reviewer_name": "squid",
+        "helpfulness": 2,
+        "photos": [
           {
-            'id': 2459023,
-            'url': 'https://static.wikia.nocookie.net/spongebob/images/9/96/The_Two_Faces_of_Squidward_174.png'
+            "id": 2459023,
+            "url": "https://static.wikia.nocookie.net/spongebob/images/9/96/The_Two_Faces_of_Squidward_174.png"
           },
           {
-            'id': 2459024,
-            'url': 'https://static.wikia.nocookie.net/spongebob/images/4/4f/The_Two_Faces_of_Squidward_075.png'
+            "id": 2459024,
+            "url": "https://static.wikia.nocookie.net/spongebob/images/4/4f/The_Two_Faces_of_Squidward_075.png"
           }
         ]
       },
       {
-        'review_id': 1275436,
-        'rating': 5,
-        'summary': 'Chester B Arthur',
-        'recommend': true,
-        'response': null,
-        'body': ' let boom = characteristicCreater(); let boom = characteristicCreater();',
-        'date': '2022-07-15T00:00:00.000Z',
-        'reviewer_name': 'johnbarleycorn',
-        'helpfulness': 0,
-        'photos': []
+        "review_id": 1275436,
+        "rating": 5,
+        "summary": "Chester B Arthur",
+        "recommend": true,
+        "response": null,
+        "body": " let boom = characteristicCreater(); let boom = characteristicCreater();",
+        "date": "2022-07-15T00:00:00.000Z",
+        "reviewer_name": "johnbarleycorn",
+        "helpfulness": 0,
+        "photos": []
       }
-    ];
+    ]
 
     request('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe')
       .get('/reviews/?product_id=37312')
@@ -94,45 +101,21 @@ describe('API calls', () => {
         if (err) {
           throw err;
         }
-      });
+      })
   });
-
 });
 
-describe('DOM Testing', () => {
+// describe('DOM Testing', () => {
 
-  afterEach(cleanup);
+//   it ('Helpful amount should increment when clicked', () => {
+//     const setState = jest.fn();
+//     const handleClick = jest.spyOn(React, 'useState');
+//     const wrapper = mount(<SingleReview />);
 
-  const review = [
-    {
-      'review_id': 1280318,
-      'rating': 3,
-      'summary': 'Thog dont caare',
-      'recommend': false,
-      'response': null,
-      'body': 'Thog dont caare',
-      'date': '2023-07-07T00:00:00.000Z',
-      'reviewer_name': 'Thog',
-      'helpfulness': 154,
-      'photos': []
-    }
-  ];
+//     handleClick.mockImplentation(count => [count, changeCount]);
+//     const button = screen.getByTestId('helpful');
+//     userEvent.click(button);
 
-  // it ('Helpful amount should increment when clicked', () => {
-  //   render(<SingleReview reviewList={review}/>);
-  //   const button = screen.getByRole('helpful');
-  //   fireEvent.click(button);
-
-  //   assert(screen.getByText('Yes (155)'), true);
-  // });
-
-  it ('Only new review button exists when product has 0 reviews', () => {
-    render(<ReviewsList />);
-    // check if more button false
-    const more = screen.queryByText('More Reviews');
-    expect(more).toBeNull();
-    // check if add button truthy
-    assert.ok(screen.getByRole('add'));
-  });
-
-});
+//     expect(changeCount).toBeCalled();
+//   });
+// });
