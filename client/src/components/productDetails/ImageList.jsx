d94541fa78163688b2a1665b98423b12d31860cc
choke <lederius.franklin@gmail.com>
2023-08-15 09:08:10 -0700

    created skeleton, creted axios call, added css file, changed prdocutDetails import in app.jsx

---
+++ client/src/components/productDetails/ImageList.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState} from 'react';
+
+export default function ImageList () {
+  // give entire banner a background color;
+
+  return (
+    <div>
+      <h3>ImageList</h3>
+    </div>
+  );
+}
