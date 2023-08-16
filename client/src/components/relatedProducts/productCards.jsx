2fc59370065c3b599aa9063e847bc6512a4cfaf6
Aslambek Baimouradov <zandak@gmail.com>
2023-08-16 11:44:53 -0400

    Fixed productCards axios

--- client/src/components/relatedProducts/productCards.jsx
+++ client/src/components/relatedProducts/productCards.jsx
@@ -1,54 +1,42 @@
-import React from 'react';
-//import './related.css';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 import RelatedList from './relatedList.jsx';
 import Stars from './StarRating';
-
-
-const ProductCard = ({styles, photo, id, actionButton, action}) => {
-  const [product, setProduct] = React.useState(null);
-  const [review, setReview] = React.useState(null);
-
-  useEffect(() => {
-    axios.get(`http://localhost:9000/products/${id}`)
-      .then(response => {
-        setProduct(response.data);
-      })
-      .catch(error =>
-        console.log('An error fetching from server:', error));
-
-    axios.get(`http://localhost:9000/reviews/meta/${id}`)
-      .then(response => {
-        const ratings = response.data.ratings;
-        var total = 0;
-        var reviewCount = 0;
-
-        for (var key in ratings) {
-          total += key * ratings[key];
-          reviewCount += parseInt(ratings[key]);
-        }
-        const averageReview = total / reviewCount;
-        setReview(averageReview);
-      })
-      .catch(error =>
-        console.log('An error fetching from server:', error));
 
 const ProductCard = ({styles, photo, id}) => {
   const [product, setProduct] = React.useState(null);
 
   useEffect(() => {
-    axios.get(`http://localhost:9000/products/${id}`)
-      .then(response => {
-        setProduct(response.data);
-      })
-      .catch(error =>
-        console.log('An error fetching from server:', error));
+    const page = 1;
+    const fetcher = (page) => {
+      if (product) {
+        return;
+      }
+      axios.get(`http://localhost:9000/products?page=${page}&count=10`)
+        .then(response => {
+          console.log('respondes', response.data)
+          for (var each of response.data) {
+            console.log('ththeeid', typeof id)
+            if (each.id === id) {
+              console.log('got one!', each.id)
+              setProduct(each);
+              return;
+            }
+          }
+          page++
+          fetcher(page);
+        })
+        .catch(error =>
+          console.log('An error fetching from server:', error));
+    };
+    fetcher(page);
+    console.log('new useeffect', product)
   }, [id]);
 
   if (!styles || !photo || !product) {
     return null;
   }
 
-  const name = styles.name;
   const price = styles.original_price;
 
 
@@ -60,7 +48,9 @@ const ProductCard = ({styles, photo, id}) => {
       </div>
       <div className='container'>
         <h2>{product.category}</h2>
+        <h2>{product.category}</h2>
         <h3><b>{product.name}</b></h3>
+        <p>${price}</p>
         <p>${price}</p>
       Rating
       </div>
