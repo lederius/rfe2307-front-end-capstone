fad90c3cce7c72bef6aa86350de780fd5271e14e
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-08-16 11:45:22 +0100

    completed form format up to characteristics

--- client/src/components/RatingReviews/NewReview.jsx
+++ client/src/components/RatingReviews/NewReview.jsx
@@ -43,9 +43,9 @@ const NewReview = ( {productID} ) => {
   return (
     <div className='form'>
       <form onSubmit={handleSubmit}>
-        {/* not sure if this works right ADD LABELS CORRESPONDING TO USER CLICK*/}
+        {/* ADD LABELS CORRESPONDING TO USER CLICK*/}
         <label>
-          Rating: <input name='rating' className='data-te-rating-init hover: fill-slate-400' onClick={(e) => console.log(e)} />
+          Rating: <input name='rating' className='' onClick={(e) => console.log(e)} />
         </label>
         <label>
           Do you recommend this product?
@@ -54,24 +54,63 @@ const NewReview = ( {productID} ) => {
         </label>
         <label>Summary: <input name='summary' maxLength={60} /></label>
         <label>Body: <input name='body' minLength={60} maxLength={100} /></label>
-        <p>
+        <div>
           Characteristics:
           <label>Size
             <div>
-              <label><input type='radio' name='sizeRadio' className='peer' />1</label><p className='invisible peer-focus:visible text-pink-600 text-sm'>A size too small</p>
-              <label><input type='radio' name='sizeRadio' className='peer' />2</label><p className='invisible peer-focus:visible text-pink-600 text-sm'>Half a size too small</p>
-              <label><input type='radio' name='sizeRadio' className='peer' />3</label>
-              <p className='invisible peer-focus:visible text-pink-600 text-sm'>Perfect</p>
-              <label><input type='radio' name='sizeRadio' className='peer' />4</label><p className='invisible peer-focus:visible text-pink-600 text-sm'>Half a size too big</p>
-              <label><input type='radio' name='sizeRadio' className='peer' />5</label><p className='invisible peer-focus:visible text-pink-600 text-sm'>A size too big</p>
+              <label><input type='radio' name='sizeRadio'/>1 - A size too small</label>
+              <label><input type='radio' name='sizeRadio' />2 - Half a size too small</label>
+              <label><input type='radio' name='sizeRadio' />3 - Perfect</label>
+              <label><input type='radio' name='sizeRadio' />4 - Half a size too big</label>
+              <label><input type='radio' name='sizeRadio' />5 - A size too big</label>
             </div>
           </label>
-          <label>Width <input /></label>
-          <label>Comfort <input /></label>
-          <label>Quality <input /></label>
-          <label>Length <input /></label>
-          <label>Fit <input /></label>
-        </p>
+          <label>Width
+            <div>
+              <label><input type='radio' name='sizeRadio' />1 - Too narrow</label>
+              <label><input type='radio' name='sizeRadio' />2 - Slightly narrow</label>
+              <label><input type='radio' name='sizeRadio' />3 - Perfect</label>
+              <label><input type='radio' name='sizeRadio' />4 - Slightly wide</label>
+              <label><input type='radio' name='sizeRadio' />5 - Too wide</label>
+            </div>
+          </label>
+          <label>Comfort
+            <div>
+              <label><input type='radio' name='sizeRadio'/>1 - Uncomfortable</label>
+              <label><input type='radio' name='sizeRadio' />2 - Slightly uncomfortable</label>
+              <label><input type='radio' name='sizeRadio' />3 - Ok</label>
+              <label><input type='radio' name='sizeRadio' />4 - Comfortable</label>
+              <label><input type='radio' name='sizeRadio' />5 - Perfect</label>
+            </div>
+          </label>
+          <label>Quality
+            <div>
+              <label><input type='radio' name='sizeRadio'/>1 - Poor</label>
+              <label><input type='radio' name='sizeRadio' />2 - Below average</label>
+              <label><input type='radio' name='sizeRadio' />3 - What I expected</label>
+              <label><input type='radio' name='sizeRadio' />4 - Pretty great</label>
+              <label><input type='radio' name='sizeRadio' />5 - Perfect</label>
+            </div>
+          </label>
+          <label>Length
+            <div>
+              <label><input type='radio' name='sizeRadio'/>1 - Runs short</label>
+              <label><input type='radio' name='sizeRadio' />2 - Runs slightly short</label>
+              <label><input type='radio' name='sizeRadio' />3 - Perfect</label>
+              <label><input type='radio' name='sizeRadio' />4 - Runs slightly long</label>
+              <label><input type='radio' name='sizeRadio' />5 - Runs long</label>
+            </div>
+          </label>
+          <label>Fit
+            <div>
+              <label><input type='radio' name='sizeRadio'/>1 - Runs tight</label>
+              <label><input type='radio' name='sizeRadio' />2 - Runs slightly tight</label>
+              <label><input type='radio' name='sizeRadio' />3 - Perfect</label>
+              <label><input type='radio' name='sizeRadio' />4 - Runs slightly loose</label>
+              <label><input type='radio' name='sizeRadio' />5 - Runs loose</label>
+            </div>
+          </label>
+        </div>
         <label>
 
         </label>
