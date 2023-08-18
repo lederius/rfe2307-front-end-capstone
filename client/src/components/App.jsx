43322a6dd15e720e66bc72550b60491fa9e2d755
Aslambek Baimouradov <zandak@gmail.com>
2023-08-18 16:11:59 -0400

    Carousel complete and axios reworked

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
       {/* <ProductDetails /> */}
       {/* <QuestionAnswerContainer /> */}
       {/* <RatingReviews /> */}
-      <RelatedProducts />
+      {/* <RelatedProducts /> */}
     </div>
   );
 };
