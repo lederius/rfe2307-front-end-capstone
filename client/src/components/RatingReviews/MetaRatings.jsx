42b60ec832e4262f3527b902604b8a210c81eed6
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-08-21 19:28:47 -0400

    calculated average rating and illustrated with stars

--- client/src/components/RatingReviews/MetaRatings.jsx
+++ client/src/components/RatingReviews/MetaRatings.jsx
@@ -1,2 +1,30 @@
+import React, { useState } from 'react';
+import StarRatings from 'react-star-ratings';
+
+const MetaRatings = ({ meta }) => {
+  console.log('meta', meta.ratings);
+
+  const average = (ratings) => {
+    let total = 0;
+    let response = 0;
+    for (let star in ratings) {
+      total += (Number(ratings[star]) * Number(star));
+      response += Number(ratings[star]);
+    }
+    const average = (total / response).toFixed(1);
+    return parseFloat(average);
+  };
+
+  return (
+    <div>
+      {meta.ratings !== undefined &&
+      <div className='font-extrabold text-5xl'>
+        {average(meta.ratings)}
+        <StarRatings rating={average(meta.ratings)} numberOfStars={5} starDimension={'32px'} starSpacing={'1px'} starRatedColor={'#FFD700'}/>
+      </div>
+      }
+    </div>
+  );
+};
 
 export default MetaRatings;
