2e7283ad186d2df72c0a193414650cc2e03f65fb
Aslambek Baimouradov <zandak@gmail.com>
2023-08-22 22:20:53 -0400

    testing start

--- client/src/components/relatedProducts/beck.test.js
+++ client/src/components/relatedProducts/beck.test.js
@@ -1,10 +1,12 @@
 import RelatedList from './relatedList.jsx';
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Comparison from './comparison.jsx';
 import RelatedProducts from './relatedProducts.jsx';
-import ProductCards from './productCards.jsx'
-;
+import ProductCards from './productCards.jsx';
+import { act } from 'react-dom/test-utils';
+
+
 describe('RelatedList component', () => {
   test('renders without errors', () => {
     render(<RelatedList />);
@@ -29,6 +31,21 @@ describe('ProductCards modal component', () => {
   });
 });
 
+describe('ProductCards modal component', () => {
+  it('Should return some text', () => {
+    act(() => {
+      render(<RelatedList displayedID={37971}/>);
+    });
+    expect(screen.getByText('Category')).toBeInTheDocument();
+  });
+});
+
+// it ('New review form should render when add review button clicked', () => {
+//   render(<ReviewsList />);
+//   const add = screen.getByRole('add');
+//   fireEvent.click(add);
+//   expect(screen.getByText('New Review')).toBeTruthy();
+// });
 // describe('Comparison card should pull product names', () => {
 //   it('shoule show', () => {
 //     expect(<Comparison mainId={37322} />).toBe('Garricl Hoodie');
