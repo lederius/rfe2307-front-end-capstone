e1b0ad6bb99d5482c77e6298e9cdb4e3bcaf382d
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-08-24 18:14:23 -0500

    add photo implementation done, fixed css

--- client/src/components/questionsAnswers/QuestionModal.jsx
+++ client/src/components/questionsAnswers/QuestionModal.jsx
@@ -20,8 +20,8 @@ const QuestionModal = ({modal, setModal, productName, productid}) => {
 
   return (
     <>
-      <div role="question-modal">
-        <div onClick={setModal} className="w-100vw h-100vh top-0 left-0 right-0 bottom-0 fixed bg-neutral-600 opacity-70 "></div>
+      <div role="question-modal" className="modal">
+        <div onClick={toggleModal} className="w-100vw h-100vh top-0 left-0 right-0 bottom-0 fixed bg-neutral-600 opacity-70 "></div>
         <div className="fixed top-36 right-2/4 translate-x-2/4 leading-6 bg-neutral-50 border-4 rounded w-96 h-124">
           <h1 className="text-xl font-bold m-2">Ask Your Question</h1>
           <p className="text-sm ml-2">About the {name}</p>
@@ -38,10 +38,10 @@ const QuestionModal = ({modal, setModal, productName, productid}) => {
             sendFormData(form);
             toggleModal();
           }}>
-            <textarea required className="m-2 h-48" placeholder="Your question"></textarea>
-            <input required className="m-2" placeholder="Example: jackson11!"></input>
+            <textarea maxLength="1000" required className="m-2 h-48 resize-none" placeholder="Your question"></textarea>
+            <input maxLength="60" required className="m-2" placeholder="Example: jackson11!"></input>
             <span className="m-2 text-xs">For privacy reasons, do not use your full name or email address</span>
-            <input required type="email" className="m-2" placeholder="Example: jack@email.com"></input>
+            <input maxLength="60" required type="email" className="m-2" placeholder="Example: jack@email.com"></input>
             <span className="m-2 text-xs">For authentication reasons, you will not be emailed</span>
 
             <button className=" m-1 py-[.688rem] px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-blue-500 hover:text-white hover:bg-blue-500 hover:border-blue-500 transition-all text-sm dark:border-gray-700 dark:hover:border-blue-500">Submit</button>
