9c842270480dc93a653fbb03e68ad4aae0a78dcb
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-08-24 20:13:03 -0400

    completed characteristics component

--- client/src/components/RatingReviews/Characteristics.jsx
+++ client/src/components/RatingReviews/Characteristics.jsx
@@ -11,7 +11,7 @@ const Characteristics = ({ chars }) => {
 
   const bar = (data) => {
     return (
-      <div className="char-bar mx-auto rounded-sm h-2 mb-4 relative" style={{ width: '100%' }}>
+      <div className="char-bar mx-auto rounded-sm h-2 mb-1 relative" style={{ width: '100%' }}>
         <div className="marker-triangle" style={marker(data.value)}></div>
       </div>
     );
@@ -24,36 +24,66 @@ const Characteristics = ({ chars }) => {
           <div>
             <p className='ml-1 mt-2 text-sm font-medium'>Size</p>
             <div>{bar(chars.Size)}</div>
+            <div className='flex justify-between text-xs'>
+              <p>Small</p>
+              <p>Perfect</p>
+              <p>Wide</p>
+            </div>
           </div>
         )}
         {chars.Width && (
           <div>
-            <p className='ml-1 text-sm font-medium'>Width</p>
+            <p className='ml-1 mt-2 text-sm font-medium'>Width</p>
             <div>{bar(chars.Width)}</div>
+            <div className='flex justify-between text-xs'>
+              <p>Narrow</p>
+              <p>Perfect</p>
+              <p>Wide</p>
+            </div>
           </div>
         )}
         {chars.Comfort && (
           <div>
-            <p className='ml-1 text-sm font-medium'>Comfort</p>
+            <p className='ml-1 mt-2 text-sm font-medium'>Comfort</p>
             <div>{bar(chars.Comfort)}</div>
+            <div className='flex justify-between text-xs'>
+              <p>Uncomfortable</p>
+              <p className='pr-12'>Ok</p>
+              <p>Perfect</p>
+            </div>
           </div>
         )}
         {chars.Quality && (
           <div>
-            <p className='ml-1 text-sm font-medium'>Quality</p>
+            <p className='ml-1 mt-2 text-sm font-medium'>Quality</p>
             <div>{bar(chars.Quality)}</div>
+            <div className='flex justify-between text-xs'>
+              <p>Poor</p>
+              <p>As Expected</p>
+              <p>Perfect</p>
+            </div>
           </div>
         )}
         {chars.Length && (
           <div>
-            <p className='ml-1 text-sm font-medium'>Length</p>
+            <p className='ml-1 mt-2 text-sm font-medium'>Length</p>
             <div>{bar(chars.Length)}</div>
+            <div className='flex justify-between text-xs'>
+              <p>Short</p>
+              <p>Perfect</p>
+              <p>Long</p>
+            </div>
           </div>
         )}
         {chars.Fit && (
           <div>
-            <p className='ml-1 text-sm font-medium'>Fit</p>
+            <p className='ml-1 mt-2 text-sm font-medium'>Fit</p>
             <div>{bar(chars.Fit)}</div>
+            <div className='flex justify-between text-xs'>
+              <p>Tight</p>
+              <p>Perfect</p>
+              <p>Loose</p>
+            </div>
           </div>
         )}
       </div>
